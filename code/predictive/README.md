5323e39c38f26d80d8039fb7aa0d06570c1ae145
Mingyi Xue <mxue33@wisc.edu>
2024-08-18 23:38:18 -0500

    add README.md to code/predictive

---
+++ code/predictive/README.md
@@ -0,0 +1,13 @@
+## Aim
+This repository is aim to practice molecule representations and predictive neural networks by predicting pKa of small molecules. The practice dataset (downloaded from [here](https://github.com/czodrowskilab/Machine-learning-meets-pKa/blob/master/datasets/combined_training_datasets_unique.sdf)) used in this repository contains 5994 molecules in total.
+
+The accuracy of literature reported pKa predictions is between 0.50 and 1 (evaluated by RMSE). Such models are trained on datasets including millions of molecules.
+
+
+## References
+- [Machine learning meets pKa](https://github.com/czodrowskilab/Machine-learning-meets-pKa)
+- [MolGpKa](https://github.com/Xundrug/MolGpKa): graph neural network
+- [SMARTS of functional groups](https://github.com/rdkit/rdkit/blob/master/Data/FunctionalGroups.txt)
+- [RDKit Fragment Module Document](https://rdkit.org/docs/source/rdkit.Chem.Fragments.html)
+- [Introduction to SMILES](https://www.daylight.com/dayhtml/doc/theory/theory.smiles.html)
+- [pkasolver](https://github.com/mayrf/pkasolver): graph isomorphism network
--- code/utils/rnn_models.py
+++ code/utils/rnn_models.py
@@ -2,11 +2,48 @@ import torch
 import torch.nn as nn
 
 class SMILESModel(nn.Module):
-    def __init__(self, mask=True, ratio=0.15):
+    def __init__(self, vocab_size, emb_dim, \
+            hidden_dim, n_layers, dropout=0.3, bidirectional=True):
         super(SMILESModel, self).__init__()
-        self.mask = mask
-        self.mask_ratio = ratio
-        raise NotImplemented
+        self.embedding = nn.Embedding(vocab_size, emb_dim)
+        self.lstm = nn.LSTM(emb_dim, hidden_dim, n_layers, \
+            dropout=dropout, batch_first=True, bidirectional=bidirectional)
+        self.dropout = nn.Dropout(dropout)
+        if bidirectional:
+            self.fc = nn.Linear(2*hidden_dim, 1)
+        else:
+            self.fc = nn.Linear(hidden_dim, 1)
     
-    def forward(self, smi_batch, smi_mask=None):
-        raise NotImplemented
+    def forward(self, token_batch):
+        out = token_batch.long()
+        out = self.embedding(out)
+        out, hidden = self.lstm(out)
+        out = out[:, -1, :]
+        out = self.dropout(out)
+        out = self.fc(out)
+        return out
+
+
+class Tokenizer():
+    def __init__(self):
+        self.vocab = ["PAD", "UNK", "Br", "Cl", "H", \
+            "N", "O", "S", "P", "F", "I", \
+            "b", "c", "n", "o", "s", "p", "[", "]", \
+            "(", ")", ".", " ", "=", "#", \
+            "+", "-", ":", "~", "@", "*", "%", \
+            "/", "\\", "0", "1", "2", "3", "4", \
+            "5", "6", "7", "8", "9"]
+        self.i2v = {i: v for i, v in enumerate(self.vocab)}
+        self.v2i = {v:i for i, v in enumerate(self.vocab)}
+        
+    def token2idx(self, v):
+        if v in self.vocab:
+            return self.v2i[v]
+        else:
+            return self.v2i["UNK"]
+    
+    def encode(self, smi):
+        lst = []
+        for v in smi:
+            lst.append(self.token2idx(v))
+        return lst
